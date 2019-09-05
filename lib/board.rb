22ffc0b5491eb27aa20740c60b65c71c20973165
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-09-05 23:41:09 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -32,7 +32,21 @@ class Board
   end
   
   def turn_count
+    cells.count{|char| char =="X" || char=="O"}
   end
+  
+  def taken?(input)
+    position(input)=="X" || position(input)=="O"
+  end
+  
+  def valid_move?(input)
+    input.to_i.between?(1, 9) && !taken?(input)
+  end
+  
+    def update(input, player)
+        cells[input.to_i-1] = player.token
+
+     end 
   
 end
 
