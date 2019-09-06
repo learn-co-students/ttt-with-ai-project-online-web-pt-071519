e827708103db01beaca3a9e32a19f75c2dcc1f1d
Thomas DiGregorio <digregoriothomas@yahoo.com>
2019-09-05 20:38:05 -0700

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,8 +1,14 @@
 module Players
     class Computer < Player  
-     def move(board)
-        (1..9).to_a.sample.to_s
-     end 
-    end 
+      def move(board)
+        position = random(board)      
+      end
+    
+      def random(board)
+        position = rand(1..9).to_s
+        board.valid_move?(position) ? position : random(board)
+      end
+    end   
+     
     
 end 
