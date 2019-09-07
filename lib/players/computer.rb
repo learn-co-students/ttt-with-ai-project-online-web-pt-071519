cf5f96fa4ef884293220690a3660c4493ec8687a
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-09-07 17:27:36 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,5 +1,14 @@
 module Players
-  class Computer < Player
+    class Computer < Player  
+      def move(board)
+        position = random(board)      
+      end
     
-  end
-end
+      def random(board)
+        position = rand(1..9).to_s
+        board.valid_move?(position) ? position : random(board)
+      end
+    end   
+     
+    
+end 
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,9 +1,10 @@
 module Players
-  class Human < Player
-    
-    def move(board)
-      puts "Please enter your selection"
-      gets.strip
+    class Human < Player
+
+       def move(board)
+        puts "Please enter your selection"
+        gets.strip
+       end
+
     end
-  end
 end
